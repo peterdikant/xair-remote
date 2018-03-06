3d6262c71d54597b3b37cc217c1e04392d5fb7d0
Peter Dikant <peter@dikant.de>
2018-03-06 21:27:22 +0100

    More work

--- osctest.py
+++ osctest.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import OSC
 import threading
 import time
@@ -5,16 +7,16 @@ import time
 # Defaults
 #XR18_ADDRESS = "192.168.178.37"
 #XR18_PORT = 10024
-XR18_ADDRESS = "192.168.77.1"
+XR18_ADDRESS = "127.0.0.1"
 XR18_PORT = 10023
-REFRESH_TIME = 4
+REFRESH_TIME = 10
 
 # Global variables
 client = None
 terminate = False
 
 def msg_handler(addr, tags, data, client_address):
-    print 'OSCMessage("%s", %s, %s)' % (addr, tags, data)
+    print 'OSCReceived("%s", %s, %s)' % (addr, tags, data)
 
 def refresh_connection():
     global terminate
@@ -22,13 +24,35 @@ def refresh_connection():
     try:
         while terminate == False:
             if client != None:
-                client.send(OSC.OSCMessage("/renew"))
-                print "Sending refresh..."
+                sendOsc("/xremote")
             time.sleep(REFRESH_TIME)
     except KeyboardInterrupt:
         exit()
     exit()
-
+    
+def sendOsc(address, param = None):
+    if client != None:
+        msg = OSC.OSCMessage(address)
+        if param != None:
+            if isinstance(param, list):
+                msg.extend(param)
+            else:
+                msg.append(param)
+        client.send(msg)
+        print 'OSCSend(%s)' % (msg)
+        
+def setupSubscriptions():
+    sendOsc("/unsubscribe")
+    for i in range(1, 17):
+        sendOsc("/subscribe", ["/ch/%02d/mix/fader" % i, 100])
+    for i in range(1, 5):
+        sendOsc("/subscribe", ["/rtn/%d/mix/fader" % i, 100])
+        sendOsc("/subscribe", ["/fxsend/%d/mix/fader" % i, 100])
+    for i in range(1, 7):
+        sendOsc("/subscribe", ["/bus/%d/mix/fader" % i, 100])
+    sendOsc("/subscribe", ["/rtn/aux/mix/fader", 100])
+    sendOsc("/subscribe", ["/lr/mix/fader", 100])
+            
 
 server = OSC.OSCServer(("", 44424))
 server.addMsgHandler("default", msg_handler)
@@ -36,10 +60,7 @@ client = OSC.OSCClient(server = server)
 client.connect((XR18_ADDRESS, XR18_PORT))
 
 # setup subscriptions
-msg = OSC.OSCMessage("/formatsubscribe")
-msg.extend(['/chmutes', '/ch/**/mix/on', 1, 16])
-client.send(msg)
-#client.send(OSC.OSCMessage("/formatsubscribe").extend(['/chmutes', '/ch/**/mix/on', 1, 16]))
+#setupSubscriptions()
 
 threading.Thread(target = refresh_connection).start()
 
@@ -47,7 +68,7 @@ threading.Thread(target = refresh_connection).start()
 try:
     server.serve_forever()
 except KeyboardInterrupt:
-    client.send(OSC.OSCMessage("/unsubscribe"))
+    sendOsc("/unsubscribe")
     terminate = True
     exit()
 
