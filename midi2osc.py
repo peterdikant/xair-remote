fa28ed63563fe2277a6130771c5b1c8b8640811f
Peter Dikant <peter@dikant.de>
2018-03-06 16:48:17 +0100

    Added subscriptions

--- midi2osc.py
+++ midi2osc.py
@@ -5,11 +5,11 @@ import time
 from mido import Message
 
 def print_ports(heading, port_names):
-	print(heading)
-	for name in port_names:
-		print("    '{}'".format(name))
-	print()
-	
+    print(heading)
+    for name in port_names:
+        print("    '{}'".format(name))
+    print()
+
 #print_ports('Input Ports:', mido.get_input_names())
 #print_ports('Output Ports:', mido.get_output_names())
 
@@ -22,16 +22,16 @@ def print_ports(heading, port_names):
 #		time.sleep(0.4)
 
 try:
-	with mido.open_input('nanoKONTROL MIDI 1') as port:
-		print('Using {}'.format(port))
-		print('Waiting for message...')
-		for message in port:
-			if message.type == 'control_change' and message.control == 2:
-				print('Sending: {}'.format(message.value / 127.0))
-				#osc_client.send_message('/ch01/mix/fader', 0.5)
-			else:
-				print('Received {}'.format(message))
-			sys.stdout.flush()
+    with mido.open_input('nanoKONTROL MIDI 1') as port:
+        print('Using {}'.format(port))
+        print('Waiting for message...')
+        for message in port:
+            if message.type == 'control_change' and message.control == 2:
+                print('Sending: {}'.format(message.value / 127.0))
+                #osc_client.send_message('/ch01/mix/fader', 0.5)
+            else:
+                print('Received {}'.format(message))
+        sys.stdout.flush()
 except KeyboardInterrupt:
-	pass
-	
+    pass
+
--- osctest.py
+++ osctest.py
@@ -3,44 +3,51 @@ import threading
 import time
 
 # Defaults
-XR18_ADDRESS = "192.168.178.37"
-XR18_PORT = 10024
-REFRESH_TIME = 3
+#XR18_ADDRESS = "192.168.178.37"
+#XR18_PORT = 10024
+XR18_ADDRESS = "192.168.77.1"
+XR18_PORT = 10023
+REFRESH_TIME = 4
 
 # Global variables
 client = None
 terminate = False
 
 def msg_handler(addr, tags, data, client_address):
-	print 'OSCMessage("%s", %s, %s)' % (addr, tags, data)
-	
+    print 'OSCMessage("%s", %s, %s)' % (addr, tags, data)
+
 def refresh_connection():
-	global terminate
-	
-	try:
-		while terminate == False:
-			if client != None:
-				#client.connect((XR18_ADDRESS, XR18_PORT))
-				client.send(OSC.OSCMessage("/xremote"))
-				print "Sending refresh..."
-			time.sleep(REFRESH_TIME)
-	except KeyboardInterrupt:
-		exit()
-	exit()
-	
-	
+    global terminate
+
+    try:
+        while terminate == False:
+            if client != None:
+                client.send(OSC.OSCMessage("/renew"))
+                print "Sending refresh..."
+            time.sleep(REFRESH_TIME)
+    except KeyboardInterrupt:
+        exit()
+    exit()
+
+
 server = OSC.OSCServer(("", 44424))
 server.addMsgHandler("default", msg_handler)
 client = OSC.OSCClient(server = server)
-client.connect(("192.168.178.37", 10024))
-#lient.send(OSC.OSCMessage("/xinfo"))
+client.connect((XR18_ADDRESS, XR18_PORT))
+
+# setup subscriptions
+msg = OSC.OSCMessage("/formatsubscribe")
+msg.extend(['/chmutes', '/ch/**/mix/on', 1, 16])
+client.send(msg)
+#client.send(OSC.OSCMessage("/formatsubscribe").extend(['/chmutes', '/ch/**/mix/on', 1, 16]))
 
 threading.Thread(target = refresh_connection).start()
 
 
 try:
-	server.serve_forever()
+    server.serve_forever()
 except KeyboardInterrupt:
-	terminate = True
-	exit()
+    client.send(OSC.OSCMessage("/unsubscribe"))
+    terminate = True
+    exit()
 
