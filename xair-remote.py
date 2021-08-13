e5696bc23c8a9a14bbf9b1095db6b5a58556b9e7
Ross Dickson <ross.dickson@astc-design.com>
2021-08-13 16:59:59 -0400

    updated main to add additional arguments and additional help text

--- xair-remote.py
+++ xair-remote.py
@@ -6,12 +6,32 @@ from lib.mixerstate import MixerState
 if __name__ == '__main__':
     PARSER = argparse.ArgumentParser(description="""
     Remote control X-Air mixers with an X-Touch midi controller.
-    """)
+
+    The X-Touch is setup so that the encoders are configured as faders on any of
+    10 banks with the first row of button configured as mutes for the selected
+    bank. Banks are selected with the buttons in the lower row and the layer
+    buttons. Most banks have two layers, the second access by pressing the
+    button a second time, causing the button to blink. The fader is used as a
+    master exit control. Pressing an encoder returns the level to unity gain,
+    not used for mic pre.
+    """,
+                                     epilog="""
+    Bank 1-6 - Aux Bus 1-6 levels for Channels 1-8 and 9-16.
+    Bank 7 - Aux Bus 7 aka FX 1 for Channels 1-8 and 9-16.
+    Bank 8 - Mic PreAmp levels for Channels 1-8 and 9-16.
+    Layer A - Main LR Mix levels of Channels 1-8 and 9-16 on second layer.
+    Layer B - Aux Bus 1-6 output levels, USB IN Gain, Main/LR Bus output level.
+    """,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
     PARSER.add_argument('xair_address', help='ip address of your X-Air mixer (optional)', nargs='?')
     PARSER.add_argument('-m', '--monitor',
                         help='monitor X-Touch connection and exit when disconnected',
                         action="store_true")
     PARSER.add_argument('-d', '--debug', help='enable debug output', action="store_true")
+    PARSER.add_argument('-l', '--levels', help='get levels from the mixer', action="store_true")
+    PARSER.add_argument('-c', '--clip', help='enabling auto leveling to avoid clipping',
+                        action="store_true")
+    PARSER.add_argument('-a', '--mac', help="use alternate mapping for mac", action="store_true")
     ARGS = PARSER.parse_args()
 
     STATE = MixerState(ARGS)
