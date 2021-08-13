6ace3cad0819424437a8ec0e97f1692668bdeaed
Ross Dickson <ross.dickson@astc-design.com>
2021-08-13 15:49:13 -0400

    clean up pylint errors, added code to cleanup state on error conditions prior to exit, moved initialization of state from main function into object, added debug printouts

--- lib/xair.py
+++ lib/xair.py
@@ -1,3 +1,4 @@
+"This modules managed communications with the XAir mixer"
 import time
 import threading
 import socket
@@ -5,15 +6,16 @@ from pythonosc.dispatcher import Dispatcher
 from pythonosc.osc_server import BlockingOSCUDPServer
 from pythonosc.osc_message import OscMessage
 from pythonosc.osc_message_builder import OscMessageBuilder
-from .mixerstate import MixerState
 
 class OSCClientServer(BlockingOSCUDPServer):
+    "The OSC communications agent"
     def __init__(self, address, dispatcher):
         super().__init__(('', 0), dispatcher)
         self.xr_address = address
 
     def send_message(self, address, value):
-        builder = OscMessageBuilder(address = address)
+        "Packs a message for sending via OSC over UDB."
+        builder = OscMessageBuilder(address=address)
         if value is None:
             values = []
         elif isinstance(value, list):
@@ -36,55 +38,93 @@ class XAirClient:
     XAIR_PORT = 10024
 
     info_response = []
-    
+
     def __init__(self, address, state):
         self.state = state
         dispatcher = Dispatcher()
         dispatcher.set_default_handler(self.msg_handler)
         self.server = OSCClientServer((address, self.XAIR_PORT), dispatcher)
-        worker = threading.Thread(target = self.run_server)
+        worker = threading.Thread(target=self.run_server)
         worker.daemon = True
         worker.start()
-    
+
     def validate_connection(self):
+        "Confirm that the connection to the XAir is live, otherwise initiaties shutdown."
         self.send('/xinfo')
         time.sleep(self._CONNECT_TIMEOUT)
         if len(self.info_response) > 0:
-            print('Successfully connected to %s with firmware %s at %s.' % (self.info_response[2], 
+            print('Successfully connected to %s with firmware %s at %s.' % (self.info_response[2],
                     self.info_response[3], self.info_response[0]))
         else:
-            print('Error: Failed to setup OSC connection to mixer. Please check for correct ip address.')
-            exit()
-        
+            print('Error: Failed to setup OSC connection to mixer.',
+                  'Please check for correct ip address.')
+            self.state.quit_called = True
+            if self.server is not None:
+                self.server.shutdown()
+                self.server = None
+            #exit()
+
     def run_server(self):
+        "Start the OSC communications agent in a seperate thread."
         try:
             self.server.serve_forever()
         except KeyboardInterrupt:
-            self.server.shutdown()
+            self.quit()
             exit()
-        
+
+    def stop_server(self):
+        if self.server is not None:
+            self.server.shutdown()
+            self.server = None
+
+    def quit(self):
+        if self.state is not None:
+            self.state.quit_called = True
+            if self.state.midi_controller is not None:
+                self.state.midi_controller.cleanup_controller()
+        self.stop_server()
+
     def msg_handler(self, addr, *data):
-            #print 'OSCReceived("%s", %s, %s)' % (addr, tags, data)
-            if addr.endswith('/fader') or addr.endswith('/on') or addr.endswith('/level') or addr.startswith('/config/mute') or addr.startswith('/fx/'):
-                self.state.received_osc(addr, data[0])
-            elif addr == '/xinfo':
-                self.info_response = data[:]
-    
-    def refresh_connection(self):
-        # Tells mixer to send changes in state that have not been recieved from this OSC Client
-        #   /xremote        - all parameter changes are broadcast to all active clients (Max 4)
-        #   /xremotefnb     - No Feed Back. Parameter changes are only sent to the active clients which didn't initiate the change
+        "Dispatch received OSC messages based on message type."
+        if self.state is None or self.state.quit_called:
+            self.stop_server()
+            return
+        #print 'OSCReceived("%s", %s, %s)' % (addr, tags, data)
+        if addr.endswith('/fader') or addr.endswith('/on') or addr.endswith('/level') or \
+                addr.startswith('/config/mute') or addr.startswith('/fx/'):
+            self.state.received_osc(addr, data[0])
+        elif addr == '/xinfo':
+            self.info_response = data[:]
+        else:         #if self.state.debug and addr.start:
+            print('OSCReceived("%s", %s)' % (addr, data))
+
+    def refresh_connection(self): # the main loop
+        """
+        Tells mixer to send changes in state that have not been received from this OSC Client
+          /xremote        - all parameter changes are broadcast to all active clients (Max 4)
+          /xremotefnb     - No Feed Back. Parameter changes are only sent to the active clients
+                                                                which didn't initiate the change
+        """
+        if self.state.debug:
+            print("Refresh Connection %s" % self.state.levels)
         try:
-            while True:
+            while not self.state.quit_called and self.server is not None:
                 self.server.send_message("/xremotenfb", None)
                 time.sleep(self._REFRESH_TIMEOUT)
+                if self.state.quit_called:
+                    return
         except KeyboardInterrupt:
+            self.quit()
             exit()
-            
-    def send(self, address, param = None):
+        except socket.error:
+            self.quit()
+
+    def send(self, address, param=None):
+        "Call the OSC agent to send a message"
         self.server.send_message(address, param)
             
 def find_mixer():
+    "Search for the IP address of the XAir mixer"
     print('Searching for mixer...')
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, True)
