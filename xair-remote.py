3f39a56b5194163c10bc1c7466d2e1b28012e447
Ross Dickson <ross.dickson@astc-design.com>
2021-08-13 15:35:14 -0400

    clean up pylint errors, moved initialization of state from main function into objects

--- xair-remote.py
+++ xair-remote.py
@@ -1,38 +1,20 @@
+"Starts an xAir remote, see main help string for more"
 #!/usr/bin/env python3
 import argparse
-import threading
-from lib.midicontroller import MidiController
-from lib.xair import XAirClient, find_mixer
 from lib.mixerstate import MixerState
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description = 'Remote control X-Air mixers with a midi controller')
-    parser.add_argument('xair_address', help = 'ip address of your X-Air mixer (optional)', nargs = '?')
-    parser.add_argument('-m', '--monitor', help='monitor X-Touch connection and exit when disconnected', action="store_true")
-    args = parser.parse_args()
+    PARSER = argparse.ArgumentParser(description="""
+    Remote control X-Air mixers with an X-Touch midi controller.
+    """)
+    PARSER.add_argument('xair_address', help='ip address of your X-Air mixer (optional)', nargs='?')
+    PARSER.add_argument('-m', '--monitor',
+                        help='monitor X-Touch connection and exit when disconnected',
+                        action="store_true")
+    PARSER.add_argument('-d', '--debug', help='enable debug output', action="store_true")
+    ARGS = PARSER.parse_args()
 
-    if args.xair_address is None:
-        address = find_mixer()
-        if address is None:
-            print('Error: Could not find any mixers in network. Please specify ip address manually.')
-            exit()
-        else:
-            args.xair_address = address
-
-    state = MixerState()
-    midi = MidiController(state)
-    state.midi_controller = midi
-    xair = XAirClient(args.xair_address, state)
-    state.xair_client = xair
-    xair.validate_connection()
-
-    if args.monitor:
-        print('Monitoring X-Touch connection enabled')
-        monitor = threading.Thread(target = midi.monitor_ports)
-        monitor.daemon = True
-        monitor.start()
-    
-    state.read_initial_state()
-    
-    # now refresh /xremote command while running
-    xair.refresh_connection()
+    STATE = MixerState(ARGS)
+    if STATE.initialize_state():
+        # now start polling refresh /xremote command while running
+        STATE.xair_client.refresh_connection()
